b0a712a339323c5612b2e750cfc6deed22329f52
Dmitry Filipuk <kupilifds@gmail.com>
2021-07-03 18:30:25 +0300

    Formatting

--- macros/Ctrl_G.lua
+++ macros/Ctrl_G.lua
@@ -1,14 +1,14 @@
 Macro {
-    description="Substitute Git reference [GitAutocomplete]";
-    area="Shell"; key="CtrlG";
-    flags="";
-    action = function()
-      pluginGuid = "89DF1D5B-F5BB-415B-993D-D34C5FFE049F"
-      pluginItemGuid = "39518AEC-DCC1-4E98-8453-6BD80F362002"
-      isPluginFound = Plugin.Menu(pluginGuid, pluginItemGuid)
-  
-      if not isPluginFound then
-        msgbox("Plugin not found", pluginGuid .. ":" .. pluginItemGuid, 0x00000001)
-      end
-    end;
-  }
+  description="Substitute Git reference [GitAutocomplete]";
+  area="Shell"; key="CtrlG";
+  flags="";
+  action = function()
+    pluginGuid = "89DF1D5B-F5BB-415B-993D-D34C5FFE049F"
+    pluginItemGuid = "39518AEC-DCC1-4E98-8453-6BD80F362002"
+    isPluginFound = Plugin.Menu(pluginGuid, pluginItemGuid)
+
+    if not isPluginFound then
+      msgbox("Plugin not found", pluginGuid .. ":" .. pluginItemGuid, 0x00000001)
+    end
+  end;
+}
